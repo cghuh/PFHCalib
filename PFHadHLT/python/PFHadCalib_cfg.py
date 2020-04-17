2e7a54baa8ac676aa0b3b6c23aef37aae4b6fc8b
cghuh <changgi.huh@cern.ch>
2020-04-17 17:54:35 +0900

    update scripts for multi-pion sample

--- PFHadHLT/python/PFHadCalib_cfg.py
+++ PFHadHLT/python/PFHadCalib_cfg.py
@@ -10,6 +10,7 @@ process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 #from PFHCalib.PFHadHLT.dump_hlt_SinglePion import *
 #from PFHCalib.PFHadHLT.step3_TrackingV2_11_0_0 import *
 from JMETriggerAnalysis.NTuplizers.hltPhase2_TRKv02_cfg import *
+from JMETriggerAnalysis.NTuplizers.tmp import *
 #from PFHCalib.PFHadHLT.hltPhase2_TRKv02_cfg import *
 
 #process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
@@ -18,7 +19,16 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10))
 process.source = cms.Source("PoolSource",
  # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-      'root://cms-xrd-global.cern.ch//store/mc/PhaseIITDRSpring19DR/SinglePion_PT0to200/GEN-SIM-DIGI-RAW/NoPU_106X_upgrade2023_realistic_v3-v2/260000/0C106524-005E-F04C-A8AC-0C2992482F4D.root'
+			#'root://cms-xrd-global.cern.ch//store/mc/Phase2HLTTDRWinter20DIGI/SinglePion_PT0to200/GEN-SIM-DIGI-RAW/NoPU_110X_mcRun4_realistic_v3-v2/50000/92FA6934-111A-F146-AD22-D50BA1BD7519.root'
+			#'root://cms-xrd-global.cern.ch//store/mc/Phase2HLTTDRWinter20DIGI/SinglePion_PT0to200/GEN-SIM-DIGI-RAW/NoPU_110X_mcRun4_realistic_v3-v2/50000/75A710FD-7A17-8B48-942F-5DA2C69201DA.root'
+			#'root://cms-xrd-global.cern.ch//store/mc/Phase2HLTTDRWinter20DIGI/SinglePion_PT0to200/GEN-SIM-DIGI-RAW/NoPU_110X_mcRun4_realistic_v3-v2/50000/DD6CC8FB-0F46-294B-BB27-7718226B5258.root'
+			#'root://cms-xrd-global.cern.ch//store/mc/Phase2HLTTDRWinter20DIGI/SinglePion_PT0to200/GEN-SIM-DIGI-RAW/NoPU_110X_mcRun4_realistic_v3-v2/50000/A41942EF-6E1C-BD4B-A092-40E46C4531C6.root'
+			#'root://cms-xrd-global.cern.ch//store/mc/Phase2HLTTDRWinter20DIGI/SinglePion_PT0to200/GEN-SIM-DIGI-RAW/NoPU_110X_mcRun4_realistic_v3-v2/50000/D13D0D95-AE8C-194D-AB2C-90E25417CDA3.root'
+			#'root://cms-xrd-global.cern.ch//store/mc/Phase2HLTTDRWinter20DIGI/SinglePion_PT0to200/GEN-SIM-DIGI-RAW/NoPU_110X_mcRun4_realistic_v3-v2/50000/E3B81E3A-9B35-A54B-A6E0-2A4D41CD83A5.root'
+			#'root://cms-xrd-global.cern.ch//store/mc/Phase2HLTTDRWinter20DIGI/SinglePion_PT0to200/GEN-SIM-DIGI-RAW/NoPU_110X_mcRun4_realistic_v3-v2/50000/0248A46E-D4D8-514D-A6F9-699E32BDF4B6.root'
+      #'file:/afs/cern.ch/work/c/chuh/PFHadCalibration/CMSSW_11_1_0_pre3/src/PFHCalib/PFHadHLT/python/0248A46E-D4D8-514D-A6F9-699E32BDF4B6.root' #single pion
+      #'file:/afs/cern.ch/work/c/chuh/PFHadCalibration/CMSSW_11_1_0_pre3/src/PFHCalib/PFHadHLT/python/47425336-6A25-5F47-ADD2-DCB8DDCADA3B.root' #multi pion
+      'file:/afs/cern.ch/work/c/chuh/PFHadCalibration/CMSSW_11_1_0_pre3/src/PFHCalib/PFHadHLT/python/33B1D528-C050-F349-91FF-B29E9E111BC9.root' #multi pion
     )
 )
 
@@ -27,10 +37,10 @@ process.pfhadhlt = cms.EDAnalyzer('PFHadHLT',
     #maxEventsToPrint = cms.untracked.int32(1)
 		   #genEvnTag = cms.InputTag("generator"),
 		   genParTag        = cms.InputTag("genParticles"),
-                   HLTPFCandidates  = cms.InputTag("RECO2","","TEST"),
-                   PFSimParticles   = cms.InputTag("PFSimParticle","","TEST"),
+                   HLTPFCandidates  = cms.InputTag("particleFlowTmp"),
+                   #PFSimParticles   = cms.InputTag("simPFProducer"),
                    #HLTPFCandidates  = cms.InputTag("hltParticleFlow","","TEST"),
-                   #PFSimParticles   = cms.InputTag("particleFlowSimParticle","","TEST"),
+                   PFSimParticles   = cms.InputTag("particleFlowSimParticle"),
 
                    ptMin = cms.double(0.01),                     # Minimum pt
                    pMin = cms.double(0.01),                      # Minimum p
@@ -54,9 +64,13 @@ process.printGenParticleList = cms.EDAnalyzer("ParticleListDrawer",
 )
 
 
+#process.reconstruction += process.simPFProducer
+process.reconstruction += process.particleFlowSimParticle
+
 #process.p = cms.Path(process.pfhadhlt)
 
 #process.AOutput = cms.EndPath( process.hltPreAOutput + process.pfhadhlt + process.printGenParticleList)
 #process.AOutput = cms.EndPath( process.hltPreAOutput + process.pfhadhlt)
 #process.AOutput = cms.EndPath( process.printGenParticleList)
 process.AOutput = cms.EndPath( process.pfhadhlt )
+process.schedule.extend([process.AOutput])
